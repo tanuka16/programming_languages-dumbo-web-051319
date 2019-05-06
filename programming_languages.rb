dc3623a0f10e42ee962b5d18deef3f02a14229d2
Tanuka Das <das.tanuka@yahoo.com>
2019-05-06 00:58:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,36 +1,5 @@
 def reformat_languages(languages)
   # your code here
-  languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
-
-  }
-}
   new_hash = []
 
   languages.each do |lan_category, lan|
